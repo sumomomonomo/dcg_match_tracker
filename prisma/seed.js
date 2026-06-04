264882dcca31a85cbc68504d9d64d24250f468d8
sumomomonomo <nmt.w1311@gmail.com>
2026-06-04 16:45:48 +0900

    fix prisma generate for render seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,8 +1,7 @@
 'use strict';
 
-import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcrypt";
-
+const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcrypt");
 const prisma = new PrismaClient();
 
 async function main() {
